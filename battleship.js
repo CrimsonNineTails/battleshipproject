6dc4c17da00400de0ab481997c5ce170bc3a3ff9
Anders Museth <anders.museth@gmail.com>
2016-09-11 19:49:33 -0700

    Added random building ships

--- battleship.js
+++ battleship.js
@@ -9,9 +9,22 @@ var collumNumber;
 var hits = 0;
 var hitsDisplay = 17;
 var fire = new Audio('Voice 001.m4a')
-var random;
+var shipsPlaced = 0;
+var placementLocation;
+var rowPlacement;
+var collumPlacement;
+var fiveBoat = 0;
+var fourBoat = 0;
+var threeBoat = 0;
+twoBoat = 0;
+var lastPlacedRow;
+var lastPlacedCollum;
+var shipSize = [2,3,3,4,5];
+var directions = [[1,0],[0,1],[-1,0],[0,-1]]
+
 // gets the container element
 var gameBoardContainer = document.getElementById("gameboard");
+
 
 // you can use this to convert your letters into numbers for use
 // with the 2D array
@@ -66,20 +79,146 @@ for (i = 0; i < cols; i++) {
 
 // Hardcoded 2D array to indicate where the ships are placed
 var gameBoard = [
-				[0,0,0,1,1,1,1,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0],
 				[0,0,0,0,0,0,0,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[0,0,0,0,0,0,1,0,0,0],
-				[1,0,0,0,0,0,1,1,1,1],
-				[1,0,0,0,0,0,0,0,0,0],
-				[1,0,0,1,0,0,0,0,0,0],
-				[1,0,0,1,0,0,0,0,0,0],
-				[1,0,0,0,0,0,0,0,0,0]
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0]
 				]
+function placeBoat(){
+			placementLocation = $('#placeBoats').val();
+			rowPlacement = letterConversion[placementLocation.substring(0,1)];
+			collumPlacement = placementLocation.substring(1,3) - 1;
+			//building of the fiveBoat
+			if(fiveBoat < 2){
+			//first part of five long boat placed
+			if(fiveBoat < 1){
+			gameBoard[rowPlacement][collumPlacement] = 1;
+			lastPlacedRow = rowPlacement;
+			lastPlacedCollum = collumPlacement;
+			fiveBoat++;
+		  }
+			else if(lastPlacedRow + 1 == rowPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement + 1][collumPlacement] = 1;
+				gameBoard[rowPlacement + 2][collumPlacement] = 1;
+				gameBoard[rowPlacement + 3][collumPlacement] = 1;
+				fiveBoat++;
+			}
+			else if (lastPlacedRow - 1 == rowPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement - 1][collumPlacement] = 1;
+				gameBoard[rowPlacement - 2][collumPlacement] = 1;
+				gameBoard[rowPlacement - 3][collumPlacement] = 1;
+				fiveBoat++;
+			}
+			else if(lastPlacedCollum + 1 == collumPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement][collumPlacement + 1] = 1;
+				gameBoard[rowPlacement][collumPlacement + 2] = 1;
+				gameBoard[rowPlacement][collumPlacement + 3] = 1;
+				fiveBoat++;
+			}
+			else if (lastPlacedCollum - 1 == collumPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement][collumPlacement - 1] = 1;
+				gameBoard[rowPlacement][collumPlacement - 2] = 1;
+				gameBoard[rowPlacement][collumPlacement - 3] = 1;
+				fiveBoat++;
+			}
+		}
+		else if (fourBoat < 2){
+			if(fourBoat < 1){
+			gameBoard[rowPlacement][collumPlacement] = 1;
+			lastPlacedRow = rowPlacement;
+			lastPlacedCollum = collumPlacement;
+			fourBoat++;
+			}
+			else if(lastPlacedRow + 1 == rowPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement + 1][collumPlacement] = 1;
+				gameBoard[rowPlacement + 2][collumPlacement] = 1;
+				fourBoat++;
+			}
+			else if (lastPlacedRow - 1 == rowPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement - 1][collumPlacement] = 1;
+				gameBoard[rowPlacement - 2][collumPlacement] = 1;
+				fourBoat++;
+			}
+			else if(lastPlacedCollum + 1 == collumPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement][collumPlacement + 1] = 1;
+				gameBoard[rowPlacement][collumPlacement + 2] = 1;
+				fourBoat++;
+			}
+			else if (lastPlacedCollum - 1 == collumPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement][collumPlacement - 1] = 1;
+				gameBoard[rowPlacement][collumPlacement - 2] = 1;
+				fourBoat++;
+			}
+		}
+		else if (threeBoat < 4) {
+			if(threeBoat < 1 || threeBoat == 2){
+			gameBoard[rowPlacement][collumPlacement] = 1;
+			lastPlacedRow = rowPlacement;
+			lastPlacedCollum = collumPlacement;
+			threeBoat++;
+			}
+			else if(lastPlacedRow + 1 == rowPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement + 1][collumPlacement] = 1;
+				threeBoat++;
+			}
+			else if (lastPlacedRow - 1 == rowPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement - 1][collumPlacement] = 1;
+				threeBoat++;
+			}
+			else if(lastPlacedCollum + 1 == collumPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement][collumPlacement + 1] = 1;
+				threeBoat++;
+			}
+			else if (lastPlacedCollum - 1 == collumPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				gameBoard[rowPlacement][collumPlacement - 1] = 1;
+				threeBoat++;
+			}
+		}
+		else if (twoBoat < 2) {
+			if(threeBoat < 1 || threeBoat == 2){
+			gameBoard[rowPlacement][collumPlacement] = 1;
+			lastPlacedRow = rowPlacement;
+			lastPlacedCollum = collumPlacement;
+			twoBoat++;
+			}
+			else if(lastPlacedRow + 1 == rowPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				threeBoat++;
+			}
+			else if (lastPlacedRow - 1 == rowPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				threeBoat++;
+			}
+			else if(lastPlacedCollum + 1 == collumPlacement){
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				twoBoat++;
+			}
+			else if (lastPlacedCollum - 1 == collumPlacement) {
+				gameBoard[rowPlacement][collumPlacement] = 1;
+				twoBoat++;
+			}
+		}
+}
 
 function fireTorpedo() {
-
 		fireLocation = document.getElementById("fireTorpedo").value;
 		console.log(fireLocation);
 		document.getElementById("fireTorpedo").value = null;
@@ -115,4 +254,64 @@ function CheckGameOver() {
 }
 function ReloadPage(){
 	location.reload();
+}
+function randomNumber(n)
+{
+	return Math.floor(Math.random() * n)
+}
+function autoMaticBoat(){
+	for(var ship = 0; ship < 5; ship++) {
+		var size = shipSize[ship];// size of the ship
+		var shipPoints = new Array(size);// grid points of ship
+		var run = true;
+		while(run){
+				var x = randomNumber(10);// first coordinate of ship
+				var y = randomNumber(10);// second coordinate of ship
+				var d = randomNumber(4);// orientation of ship
+				for (var i = 0; i < size ; i++) {// loop over points of ship
+					var p = x + directions[d][0] * i;
+					var q = y + directions[d][1] * i;
+					shipPoints[i] = [p,q];// add point to ship
+				}
+				if (checkPoints(shipPoints) == 1) {// check if points are already used
+					setPoints(shipPoints);// set point to one on the grid
+					run = false;// done with this ship, move on to the next
+				}
+		}
+	}
+	/*
+	console.log(gameBoard[0]);
+	console.log(gameBoard[1]);
+	console.log(gameBoard[2]);
+	console.log(gameBoard[3]);
+	console.log(gameBoard[4]);
+	console.log(gameBoard[5]);
+	console.log(gameBoard[6]);
+	console.log(gameBoard[7]);
+	console.log(gameBoard[8]);
+	console.log(gameBoard[9]);
+	*/
+}
+function checkPoints(points){
+	for (var i = 0; i < points.length; i++) {
+		var x = points[i][0];
+		if (x < 0 || x > 9) {
+			return 0;
+		}
+		var y = points[i][1];
+		if( y < 0 || y > 9){
+			return 0;
+		}
+		if (gameBoard[x][y] == 1) {
+			return 0;
+		}
+	}
+	return 1;
+}
+function setPoints(points){
+	for (var i = 0; i < points.length; i++) {
+		var x = points[i][0];
+		var y = points[i][1];
+		gameBoard[x][y] = 1;
+	}
 }
